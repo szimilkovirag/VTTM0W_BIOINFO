7ea54b485881cd002469ad52b57608456190d276
Szimilkó Virág <szimiviri11@stud.uni-obuda.hu>
2022-11-24 20:38:20 +0100

    Create an mRNS handler

---
+++ messenger_rns_handler.py
@@ -0,0 +1,11 @@
+import argparse
+
+
+class MessengerRnsHandler:
+    __init__(dna):
+        self.dna = dna
+
+if __name__ == '__main__':
+    arg_parser = argparse.ArgumentParser()
+    
+    arg_parser.add_argument()
