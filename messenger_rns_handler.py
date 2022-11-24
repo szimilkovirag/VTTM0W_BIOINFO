d8065aed34e6505fd5c977344a548f732825f508
FIXED-TERM Szimilko Virag (XC/ENG4-Bp) <fixed-term.virag.szimilko@hu.bosch.com>
2022-11-24 21:17:31 +0100

    fixup! Create an mRNS handler

--- messenger_rns_handler.py
+++ messenger_rns_handler.py
@@ -2,10 +2,41 @@ import argparse
 
 
 class MessengerRnsHandler:
-    __init__(dna):
+    def __init__(self, dna):
         self.dna = dna
+
+    def run(self):
+        self.transcription(self.dna)
+
+    def transcription(self, dna):
+        '''Create a messenger RNS from a branch of the DNA according to these rules:
+            - T --> A
+            - A --> U
+            - G --> C
+            - C --> G'''
+        rns = ''
+        for base in dna:
+            if base == 'T':
+                rns = rns + 'A'
+            elif base == 'A':
+                rns = rns + 'U'
+            elif base == 'G':
+                rns = rns + 'C'
+            elif base == 'C':
+                rns = rns + 'G'
+
+        return rns
+
 
 if __name__ == '__main__':
     arg_parser = argparse.ArgumentParser()
-    
-    arg_parser.add_argument()
+
+    arg_parser.add_argument('-dna', '--deoxy_ribonucleic_acid',
+                            default="",
+                            help='This is a string which defines the DNA chain. It consists of base elements(A, T, C, G)')
+
+    args = arg_parser.parse_args()
+
+    handler = MessengerRnsHandler(args.deoxy_ribonucleic_acid)
+
+    handler.run()
